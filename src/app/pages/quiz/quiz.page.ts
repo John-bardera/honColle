e42aa401fef7085461c0fc7144748466cf5bceaa
Asada1129 <44834263+Asada1129@users.noreply.github.com>
2020-07-29 14:10:59 +0900

    quiz画面だけ

--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -7,14 +7,50 @@ import { Component } from '@angular/core';
 })
 export class QuizPage {
 
+  itemList=[];
+
   selectedtab: string;
   selectedQ: string;
   selectedmake: string;
 
   constructor() {
-    this.selectedtab = 'make';
+    this.selectedtab = 'challenge';
     this.selectedQ = '0';
-    this.selectedmake = '3';
+    this.selectedmake = '0';
+  }
+
+  ionViewWillEnter(){
+    this.itemList = [
+      'アクタージュのクイズ','メイドインアビスのクイズ','ごとうぶんの花嫁クイズ',
+      'ご注文はうさぎですか？クイズ'
+    ];
+  }
+
+  async getItems(ev: any){
+    let value = ev.target.value; //イベントを発生させたオブジェクトのvalueつまり入力した文字
+    if(value === ''){
+      this.itemList = [
+        'アクタージュのクイズ','メイドインアビスのクイズ','ごとうぶんの花嫁クイズ',
+        'ご注文はうさぎですか？クイズ'
+      ];
+    }
+    // if the value is an empty string don't filter the items
+    if(value !== ''){
+      this.itemList = this.itemList.filter((item) => {
+        //前方一致
+        /*if(value.match(/^[A-Za-z0-9]*$/)){
+          return (item.toString().toLowerCase().indexOf(value.toLowerCase()) === 0);
+        }else{
+          return (item.toString().toLowerCase().indexOf(value.toLowerCase()) === 0);
+        }*/
+        //部分一致
+        if (value.match(/^[A-Za-z0-9]*$/)) {
+          return (item.toLowerCase().indexOf(value));
+        } else {
+          return (item.toLowerCase().indexOf(value));
+        }
+      });
+    }
   }
 
   onSelectTab(tab: string){
