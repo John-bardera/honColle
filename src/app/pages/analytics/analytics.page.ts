49628ceadd3745b621325cb900331e435594afed
Asada1129 <44834263+Asada1129@users.noreply.github.com>
2020-08-09 19:28:56 +0900

    エラー出てます

--- src/app/pages/analytics/analytics.page.ts
+++ src/app/pages/analytics/analytics.page.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-
-import Chart from 'chart.js';
-
+import { Component, } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Router } from '@angular/router';
 
 import { Book } from '@/models';
 import { AppState } from '@/store';
@@ -15,78 +15,26 @@ import { selectBooks } from '@/store/book.store';
   styleUrls: ['analytics.page.scss'],
 })
 export class AnalyticsPage {
-  @ViewChild('MyChart', { static: true }) myChart;
-
-  selected: string;
-
-    // グラフの表示サイズ
-  view: any[] = [700, 400];
-
-  // 設定
-  showLegend = true;
-  showLabels = true;
-  explodeSlices = false;
-  doughnut = false;
-
-　// カラーテーマ
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-
-  // サンプルデータ
-  sampleData = [
-    {name: '読書済み', value: '13'},
-    {name: '積読', value: '48'},
-    {name: '未所持', value: '39'}
-  ];
-
-  constructor() {
-    this.selected = 'author';
+  author$: Observable<Array<String>>;
+  constructor(
+    private router: Router,
+    private store: Store<AppState>,
+  ) {
+    this.author$ = this.store.pipe(
+      select(selectBooks),
+      map(books => {
+        const author = [];
+        books.map(book => {
+          if(!author.includes(book.author)){
+            author.push(book.author);
+          }
+          return author;
+        })
+      })
+    )
   }
 
-  ionViewDidEnter() {
-    const chart = new Chart(this.myChart.nativeElement, {
-      type: 'pie',
-      data: {
-        labels: ['赤', '青', '黄', '緑', '紫', '橙'],
-        datasets: [{
-          label: '得票数',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-  segmentChanged(ev: any) {
-    console.log(ev);
-  }
-  onSelectTab(tab: string) {
-    this.selected = tab;
+  onChangePage(){
+    this.router.navigateByUrl('tabs/analytics/graph');
   }
 }
