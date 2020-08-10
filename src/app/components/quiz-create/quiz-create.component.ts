23dd0f02757bab70e694482e08e896839d08e4c7
john <johnbardera@gmail.com>
2020-08-10 18:09:55 +0900

    finished quiz-create.component

--- src/app/components/quiz-create/quiz-create.component.ts
+++ src/app/components/quiz-create/quiz-create.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
 import { Store } from '@ngrx/store';
 
-import { Book, Quiz } from '@/models';
+import { Book, Question, Quiz } from '@/models';
 import { AppState } from '@/store';
 import { setQuiz } from '@/store/quiz.store';
 
@@ -13,23 +14,86 @@ import { setQuiz } from '@/store/quiz.store';
 export class QuizCreateComponent implements OnInit {
   @Input() theme: Book;
   quiz: Quiz;
-  selectedQuestionIndex: number;
+  newQuestion: Question;
+  selectedQuestionIndex = -1;
   creatingQuestion = false;
 
   constructor(
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
     private store: Store<AppState>,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.quiz =  {
+      book: this.theme,
+      comments: [],
+      id: '',
+      maker: '',
+      questions: [],
+      title: '',
+    };
+    this.initQuestion();
+  }
 
+  initQuestion() {
+    this.newQuestion =  {
+      choices: ['', '', ''],
+      correct: 0,
+      text: '',
+    };
+  }
   touchQuestion() {
     this.creatingQuestion = true;
+    this.initQuestion();
+  }
+  checkQuestion(): boolean {
+    return this.newQuestion.correct &&
+      this.newQuestion.text &&
+      this.newQuestion.choices[0] &&
+      this.newQuestion.choices[1] &&
+      this.newQuestion.choices[2] && true;
   }
   addedQuestion() {
     this.creatingQuestion = false;
+    this.newQuestion.correct--;
+    this.quiz.questions.push(this.newQuestion);
   }
   createdQuiz() {
     this.quiz.id = this.quiz.maker + Math.round( Math.random() * 10000000 );
     this.store.dispatch(setQuiz({ quiz: this.quiz }));
+    console.log(this.quiz);
+    this.dismiss();
+  }
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalCtrl.dismiss({
+      dismissed: true
+    });
+  }
+  async irregularDismiss() {
+    const alert = await this.alertCtrl.create({
+      message: '途中で終了しても良いですか？\nまだ保存されていません。',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => this.dismiss(),
+        },
+        {
+          text: 'キャンセル',
+          handler: () => {}
+        }
+      ]
+    });
+    await alert.present();
+  }
+  selectQuestion(i: number) {
+    this.selectedQuestionIndex = this.selectedQuestionIndex === i ? -1 : i;
+  }
+  checkQuiz(): boolean {
+    return this.quiz.maker &&
+      this.quiz.title &&
+      this.quiz.questions.length && true;
   }
 }
--- src/app/pages/quiz/quiz-create-search/quiz-create-search.page.ts
+++ src/app/pages/quiz/quiz-create-search/quiz-create-search.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { Book } from '@/models';
+import { ClickedButtonParams } from '@/components/list-item/list-item.component';
+import { QuizCreateComponent } from '@/components/quiz-create/quiz-create.component';
+import { Book, Quiz } from '@/models';
 import { BookService } from '@/services';
 import { AppState } from '@/store';
 import { selectBooks } from '@/store/book.store';
@@ -16,6 +19,7 @@ export class QuizCreateSearchPage implements OnInit {
   searchedBooks$: Observable<Array<Book>>;
 
   constructor(
+    private modalCtrl: ModalController,
     private store: Store<AppState>,
     private bookService: BookService,
   ) {
@@ -28,7 +32,13 @@ export class QuizCreateSearchPage implements OnInit {
     const value = ev.target.value; // イベントを発生させたオブジェクトのvalueつまり入力した文字
     this.searchedBooks$ = value ? this.bookService.searchFromLocalStore(value) : this.store.pipe(select(selectBooks));
   }
-  onMake(make: string) {
-    return;
+  async clickedButtons(ev: ClickedButtonParams) {
+    const modal = await this.modalCtrl.create({
+      component: QuizCreateComponent,
+      componentProps: {
+        theme: ev.content as Book,
+      },
+    });
+    await modal.present();
   }
 }
